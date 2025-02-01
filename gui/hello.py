6b10fdb6ca516edf0820b12f28ae6aa38c38eeb0
Matt Harrison <matt@macmini.tail904b6f.ts.net>
2025-02-01 12:47:00 +0000

    bidirectional comms working

--- gui/hello.py
+++ gui/hello.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from gui!")
 
+import serial
+import msgpack
+import time
+import json
+i = 0
+with serial.Serial('/dev/cu.usbmodem2101', 115200, timeout=10) as ser:
+    while True:
+        # i += 1
+        
+        # ser.write(msgpack.packb([18, f'hello {i}\n'.encode()]))
+        # out = b''
+        # while ser.inWaiting() > 0:
+        #     out += ser.read(100)
 
-if __name__ == "__main__":
-    main()
+        before = time.perf_counter()
+        ser.write(json.dumps({'command': 'respond', 'key': i}).encode())
+        i+=1
+
+        out = ser.read_until(b'}')
+        if (len(out)):
+            after = time.perf_counter()
+            print("got", out, after - before)
+        # unpacker = msgpack.Unpacker(ser)
+        # for unpacked in unpacker:
+        #     # print(unpacked)
